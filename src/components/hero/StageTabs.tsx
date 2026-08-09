d8eff4a53012239e8d5941addba85636e1c49cb6
hamad933 <hamadco933@gmail.com>
2026-08-09 20:08:54 +0300

    fix: remove unused React import from StageTabs

--- src/components/hero/StageTabs.tsx
+++ src/components/hero/StageTabs.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 import { CheckIcon, ChevronLeftIcon, PauseIcon, PlayIcon } from 'lucide-react';
 import { STAGE_ORDER, STAGE_COPY } from '../../data/heroContent';
 
