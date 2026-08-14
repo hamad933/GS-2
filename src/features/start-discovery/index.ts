2d0079f7a34d6a136b5e1fd4739afccc875c9ad3
hamad933 <hamadco933@gmail.com>
2026-08-14 03:08:00 +0300

    feat(start-discovery): build adaptive project intake body

---
+++ src/features/start-discovery/index.ts
@@ -0,0 +1,20 @@
+export { StartDiscoveryBody } from './StartDiscoveryBody';
+export {
+  buildDiscoverySummary,
+  createStartDiscoveryDraft,
+  formatDiscoverySummary,
+  getDiscoverySteps,
+} from './discoveryModel';
+export type {
+  DiscoveryCertainty,
+  DiscoveryPrefillSource,
+  DiscoveryStepId,
+  DiscoverySummary,
+  DiscoverySummaryGroup,
+  DiscoverySummaryItem,
+  DiscoverySummaryStatus,
+  StartDiscoveryBodyProps,
+  StartDiscoveryDraft,
+  StartDiscoveryPrefill,
+} from '../../types/start-discovery';
+export { START_DISCOVERY_PREFILL_VERSION } from '../../types/start-discovery';
