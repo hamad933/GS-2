03368026271aad01bb3747052b4c7a1743b189bb
hamad933 <hamadco933@gmail.com>
2026-08-23 18:46:06 +0300

    test(control): reject undocumented Jules cancel states

--- .project-control/tests/test_state_matcher.py
+++ .project-control/tests/test_state_matcher.py
@@ -87,6 +87,8 @@ class Matrix(unittest.TestCase):
         self.assertNotEqual(review_decision(verdict=None,reviewed_sha=CAND,candidate_sha=CAND).state,ReviewState.PASS_FOR_PARENT_REVIEW)
     def test_35_unknown_future_state_escalates(self):
         self.assertEqual(normalize_jules("FUTURE_WEIRD"),JulesState.UNKNOWN_JULES_STATE)
+        self.assertEqual(normalize_jules("CANCELLED"),JulesState.UNKNOWN_JULES_STATE)
+        self.assertEqual(normalize_jules("CANCELED"),JulesState.UNKNOWN_JULES_STATE)
         self.assertEqual(normalize_ci("FUTURE_WEIRD"),CIState.UNKNOWN_CI_STATE)
     def test_36_matcher_weakness_is_not_self_modification(self):
         d=waiting_decision({"unexpected_new_condition":True})
